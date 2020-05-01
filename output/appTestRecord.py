c7c1befa1ee53cbfa3b7a2719df24cd402d674de
k-eeer <37653119+k-eeer@users.noreply.github.com>
2020-05-01 19:31:50 +0800

    Delete appTestRecord.py

--- output/appTestRecord.py
+++
@@ -1,9 +0,0 @@
-el3 = driver.find_element_by_id("info.woodsmall.calculator:id/btn9")
-el3.click()
-el4 = driver.find_element_by_id("info.woodsmall.calculator:id/btnPlus")
-el4.click()
-el5 = driver.find_element_by_id("info.woodsmall.calculator:id/btn9")
-el5.click()
-el6 = driver.find_element_by_id("info.woodsmall.calculator:id/btnEqual")
-el6.click()
-
